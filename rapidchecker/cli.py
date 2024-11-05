d11ae39f4d7a96c0117d25e8715b1c0800076766
OdysseasKr <odysseaskr@users.noreply.github.com>
2024-11-05 11:16:04 +0100

    Lint

--- rapidchecker/cli.py
+++ rapidchecker/cli.py
@@ -1,19 +1,20 @@
 import sys
-from rich import print
+
 import click
+from rich import print
 
 from .check import check_format
 from .io import get_sys_files, read_sys_file
 
 
-def in_ignore_list(path: str, ignore_list: list[str]):
+def in_ignore_list(path: str, ignore_list: list[str]) -> bool:
     return any(item in path for item in ignore_list)
 
 
 @click.command()
 @click.argument("path", type=click.Path(exists=True))
 @click.option("--ignore", multiple=True)
-def cli(path: str, ignore: list[str]):
+def cli(path: str, ignore: list[str]) -> None:
     found_errors = False
 
     for filepath in get_sys_files(path):
@@ -28,5 +29,5 @@ def cli(path: str, ignore: list[str]):
                 print("\t", error)
 
     if not found_errors:
-        print(":heavy_check_mark: ", "No RAPID format errors found!")  # noqa
+        print(":heavy_check_mark: ", "No RAPID format errors found!")
     sys.exit(found_errors)
--- rapidchecker/io.py
+++ rapidchecker/io.py
@@ -1,15 +1,16 @@
-import glob
-from typing import Iterable
+from collections.abc import Iterable
+from pathlib import Path
 
 
-def get_sys_files(path: str) -> Iterable[str]:
+def get_sys_files(path: str | Path) -> Iterable[str]:
+    path = Path(path)
     if path.endswith(".sys"):
         return [path]
     if path.endswith("/"):
         path = path[:-1]
-    return glob.iglob(path + "/**/*.sys", recursive=True)
+    return path.glob("**/*.sys")
 
 
-def read_sys_file(path: str) -> list[str]:
-    with open(path) as f:
+def read_sys_file(path: str | Path) -> list[str]:
+    with Path(path).open() as f:
         return f.read()
