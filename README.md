768a687fcff8048ce54fc4501e96c305de54a4d6
OdysseasKr <odysseaskr@users.noreply.github.com>
2024-11-05 11:30:52 +0100

    Use pyprocessing exception attributes instead of manual formatting

--- README.md
+++ README.md
@@ -28,7 +28,6 @@ If any grammar or format errors are found, they are printed to stdout and the co
 ## To be added
 
 - Add package to pip.
-- Automated tests.
 - Whitespace checks (trailing space, blank line at EOF)
 - File-based customization of rules (indentation size, rule ignore list etc).
 - Checks for procedure, variable, function and signal names (enforce camel_case or snakeCase).
--- rapidchecker/check.py
+++ rapidchecker/check.py
@@ -4,22 +4,8 @@ from .parser.grammar import module
 from .parser.indent import reset_level
 
 
-def format_exception_message(e: pp.ParseSyntaxException) -> str:
-    return f"{e.msg}{foundstr(e)}"
-
-
-def foundstr(e: pp.ParseSyntaxException) -> str:
-    if not e.pstr:
-        return ""
-
-    if e.loc >= len(e.pstr):
-        return ", found end of text"
-    # pull out next word at error location
-    found_match = pp.exceptions._exception_word_extractor.match(e.pstr, e.loc)
-    found = (
-        found_match.group(0) if found_match is not None else e.pstr[e.loc : e.loc + 1]
-    )
-    return (f", found {found!r}").replace(r"\\", "\\")
+def get_exception_message(e: pp.ParseSyntaxException) -> str:
+    return f"{e.lineno}:{e.column} {e.msg}, found {e.found}"
 
 
 def check_format(content: str) -> list[pp.ParseSyntaxException]:
@@ -28,6 +14,6 @@ def check_format(content: str) -> list[pp.ParseSyntaxException]:
     try:
         module.parseString(content, parseAll=True)
     except pp.ParseSyntaxException as e:
-        # TODO: Fix message formatting here
-        errors.append(f"{e.lineno}:{e.column} {format_exception_message(e)}")
+        message = get_exception_message(e)
+        errors.append(message)
     return errors
