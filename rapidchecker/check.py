3d8d062c1a291c412efbf4bae5ad5c7fb830034d
OdysseasKr <odysseaskr@users.noreply.github.com>
2024-11-06 01:10:40 +0100

    Monkey patch __str__ of parse errors

--- rapidchecker/check.py
+++ rapidchecker/check.py
@@ -8,12 +8,14 @@ def get_exception_message(e: pp.ParseSyntaxException) -> str:
     return f"{e.lineno}:{e.column} {e.msg}, found {e.found}"
 
 
+pp.ParseSyntaxException.__str__ = get_exception_message
+
+
 def check_format(content: str) -> list[pp.ParseSyntaxException]:
     reset_level()
     errors = []
     try:
         module.parseString(content, parseAll=True)
     except pp.ParseSyntaxException as e:
-        message = get_exception_message(e)
-        errors.append(message)
+        errors.append(e)
     return errors
