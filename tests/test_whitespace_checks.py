ca9bc4d88bb398007b1480953dd5f35dc1c4f430
OdysseasKr <odysseaskr@users.noreply.github.com>
2024-11-11 00:12:29 +0100

    Fix tests

--- tests/test_whitespace_checks.py
+++ tests/test_whitespace_checks.py
@@ -17,28 +17,27 @@ def test_check_whitespace_no_errors() -> None:
 def test_check_whitespace_with_errors() -> None:
     file_contents = Path("./tests/rapid_samples/BadWhitespaceModule.sys").read_text()
     result = check_whitespace(file_contents)
-    print(result)
     expected = [
         TrailingSpaceError(
-            line=4,
+            lineno=4,
             col=26,
             error="Line ends with space",
             suggestion="Remove trailing spaces",
         ),
         TooManyEmptyLinesError(
-            line=7,
+            lineno=7,
             col=0,
             error="Too many empty lines",
             suggestion="Use up to 2 empty lines",
         ),
         TooManyEmptyLinesError(
-            line=8,
+            lineno=8,
             col=0,
             error="Too many empty lines",
             suggestion="Use up to 2 empty lines",
         ),
         NoTrailingLineError(
-            line=27,
+            lineno=27,
             col=0,
             error="Line does not end with a new line character",
             suggestion="Add a new line character at the end of the line",
